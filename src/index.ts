06170a4a81f30b783ce4dbf5fb124a3058a1aae1
SachaCR <sacha@spendesk.com>
2022-08-11 23:28:46 +0200

    fix documentation and export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './SchottottenGame';
+export { CardDetails, CardColor, cardColors } from './Card';
+export { PlayerState } from './Player';
+export { BoundaryMarkerState, BoundaryMarkerOwner } from './BoundaryMarker';
